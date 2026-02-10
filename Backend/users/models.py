05531bc6cadb1b6c5a087c646ca807c90757901b
anthonybanion <abanon@fi.uba.ar>
2026-02-10 11:51:22 -0300

    [Backend]=> Usamos username y no email

--- Backend/users/models.py
+++ Backend/users/models.py
@@ -3,15 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    # Usamos email como identificador único para un login más moderno
-    # y evitar duplicar username + email.
-    username = None
-    email = models.EmailField(unique=True)
+
     profile_image = models.URLField(blank=True, null=True)
 
-    # Timestamps para auditoría y trazabilidad de usuarios
+    # Timestamps para auditoría básica
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
