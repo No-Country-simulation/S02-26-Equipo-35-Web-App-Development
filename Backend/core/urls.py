a4e4eec94cfa08d8e0b3c943d03e80c289bb8410
Test User <test@example.com>
2026-02-12 00:54:23 -0500

    feat: add DRF Spectacular for API documentation

--- Backend/core/urls.py
+++ Backend/core/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # API v1 - Main endpoints  
+    path('api/shorts/', include('shorts.urls')),
 ]
