0fc99fe6615bc4a557254111384ee0d323a8b2e9
Anthony Josue Erazo Llacsahuanga <124642443+AnthonyErazo@users.noreply.github.com>
2026-02-14 10:22:37 -0500

    Configure CORS settings in settings.py

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -21,6 +21,12 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True
 ALLOWED_HOSTS = ["*"]
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = False
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^http://localhost(:\d+)?$",
+    r"^http://127\.0\.0\.1(:\d+)?$",
+    r"^https?://.*$",
+]
 
 # ===========================
 # APPS
@@ -52,6 +58,7 @@ INSTALLED_APPS = [
 # MIDDLEWARE
 # ===========================
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
