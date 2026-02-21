3fac35685c2c8f2f151c15795ebc007a25e409c8
M4x241 <maxrodaspc@gmail.com>
2026-02-21 18:35:47 -0400

    Implementacion de modelo Gemini y generacion de Shorts

--- Backend/videos/audio_processor.py
+++ Backend/videos/audio_processor.py
@@ -1,6 +1,5 @@
 import whisper
-import google.generativeai as genai
-from dotenv import load_dotenv
+from google import genai
 import os
 import logging
 import json
@@ -17,16 +16,15 @@ class AudioProcessor:
     """
 
     def __init__(self):
-        load_dotenv()
 
-        self.api_key = os.getenv("API_KEY_GEMINI")
+        self.api_key = "AIzaSyBc0kZWvHCaQ0Cdv6Tj0y3kl8GpKGEyRGA"
+
         if not self.api_key:
-            raise ValueError("API_KEY_GEMINI no configurada en .env")
+            raise ValueError("API_KEY_GEMINI no configurada")
 
-        genai.configure(api_key=self.api_key)
+        self.client = genai.Client(api_key=self.api_key)
 
         self.whisper_model = whisper.load_model("base")
-        self.gemini_model = genai.GenerativeModel("gemini-2.5-flash")
 
         logger.info("AudioProcessor configurado correctamente")
 
@@ -39,6 +37,7 @@ class AudioProcessor:
             raise FileNotFoundError(f"Video no encontrado: {video_path}")
 
         logger.info("Transcribiendo video con Whisper")
+        logger.info(f"✅ Paso 1")
 
         result = self.whisper_model.transcribe(
             video_path,
@@ -50,7 +49,7 @@ class AudioProcessor:
 
         if not segments:
             raise ValueError("No se detectó audio o texto en el video")
-
+        logger.info(f"✅ Paso 2")
         formatted_segments = [
             {
                 "start": round(seg["start"], 2),
@@ -61,7 +60,7 @@ class AudioProcessor:
         ]
 
         logger.info(f"Transcripción completada con {len(formatted_segments)} segmentos")
-
+        logger.info(f"✅ Paso 3")
         return formatted_segments
 
     def analyze_segments_with_gemini(self, segments):
@@ -78,10 +77,8 @@ class AudioProcessor:
 
         prompt = f"""
 Analiza los siguientes segmentos de un video con marcas de tiempo.
-
 Selecciona los fragmentos más interesantes para crear clips cortos virales.
-
-Devuelve SOLO un JSON válido en este formato:
+Devuelve SOLO 3 JSON válido en este formato:
 
 [
   {{
@@ -98,12 +95,23 @@ Si no hay clips relevantes, devuelve:
 Segmentos:
 
 {segments_text}
+
+Y solo debes devolver lo que te pido nada mas, ningun punto extra ni un entendido.
 """
 
-        response = self.gemini_model.generate_content(prompt)
+        response = self.client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=prompt,
+        )
+
         response_text = response.text.strip()
 
+
         try:
+            response_text = response_text.strip().removeprefix("```json").removesuffix("```").strip()
+            # print("=====================================================")
+            # print(response_text)
+            # print("=====================================================")
             clips = json.loads(response_text)
             return clips
         except json.JSONDecodeError:
--- Backend/videos/extractordeaudio.py
+++ Backend/videos/extractordeaudio.py
@@ -1,27 +1,10 @@
-import whisper
-import google.generativeai as genai
-from dotenv import load_dotenv
-import os
+from audio_processor import AudioProcessor
 
-load_dotenv()
-model = whisper.load_model("base")
-genai.configure(api_key=os.getenv('API_KEY_GEMINI'))
-print("API Key de Gemini configurada correctamente." , os.getenv('API_KEY_GEMINI'))
-# result = model.transcribe("/home/ghost/Videos/audio.wav")
-result = model.transcribe("/home/ghost/Downloads/audio.wav")
+print("Iniciando")
 
-print(result["text"])
+temp_video_path = "/home/ghost/Downloads/Esta IA Es Un PELIGRO.mp4"
 
-segments_text = " ".join([segment["text"] for segment in result["segments"]])
-
-prompt = f"""Te paso un texto de los datos extraidos del audio de un video, lo que debes hacer ahora es devolver en un json, los datos de segundo de inicio y segundo donde podria finalizar un corte de un clip, si no se encuentra ningun clip que sea relevante, enviar un formato de respuesta con mensaje de error no valido en json, solo envia el json, no respondas nada mas:
-
-Texto: {segments_text}"""
-
-# model_gemini = genai.GenerativeModel("gemini-pro")
-# model_gemini = genai.GenerativeModel("gemini-1.5-flash")
-# model_gemini = genai.GenerativeModel("gemini-2.0-flash")
-model_gemini = genai.GenerativeModel("gemini-2.5-flash")
-response = model_gemini.generate_content(prompt)
-
-print(response.text)
+audio = AudioProcessor()
+clips_data = audio.process_video(temp_video_path)
+print(clips_data)
+print("finalizado")
